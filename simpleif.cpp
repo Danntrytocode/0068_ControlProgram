693e89ab0b61a70351e1a8f6dab699b2d8a56ff5
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-03-13 22:22:33 +0700

    menambahkan program utama yang berisi simple if

--- simpleif.cpp
+++ simpleif.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+    int x;
+
+    srand(time(0));
+    x = srand() %10;
+
+    if (x==5){
+        x = x+1;
+        cout <<"Nilai x Sudah Dikoreksi Menjadi : "<<x<<endl;
+    }
+
+    cout << "Bilangan yang di Berikan = "<<x;
+}
