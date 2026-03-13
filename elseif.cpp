193971a9794d868e9bc3c7662afcc6cabca21e06
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-03-13 22:03:27 +0700

    menambahkan program utama

--- elseif.cpp
+++ elseif.cpp
@@ -11,3 +11,25 @@ string Status_lulus(float r){
     else
         return "Gagal";
 }
+
+int main (){
+    float nilBI,nilMT,rerata;
+    string status;
+
+    cout << "Masukkan Nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout << "Masukkan Nilai Matematika = ";
+    cin >> nilMT;
+
+
+    //rerata = (nilBI + nilMT)/2;
+    //if (rerata >= 60)
+    //    status = "Lolos";
+    //else
+        //  status = "Gagal";
+    rerata = Rata_rata(nilBI,nilMT);
+    //status = Status_lulus(rerata);
+
+    cout << "Status Kelulusan = " <<Status_lulus(rerata)<<" ,dengan Nilai Rerata = " << rerata<<endl;
+
+}
